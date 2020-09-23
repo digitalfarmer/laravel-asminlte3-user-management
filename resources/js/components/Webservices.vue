e1430cf84b80a7a58c85a1898591a6a562c3dcce
Ades <ades@binasanprima.com>
2020-09-23 14:45:06 +0700

    configuring vuejs laravel

---
+++ resources/js/components/Webservices.vue
@@ -0,0 +1,93 @@
+<template >
+    <!-- <div class="container"> -->
+         <div class="row ">
+            <div class="col-lg-3">
+                <!-- Apply any bg-* class to to the info-box to color it -->
+                <div class="info-box bg-red">
+                <span class="info-box-icon"><i class="fa fa-comments"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Sales</span>
+                    <span class="info-box-number">41,410</span>
+                    <!-- The progress section is optional -->
+                    <div class="progress">
+                    <div class="progress-bar" style="width: 70%"></div>
+                    </div>
+                    <span class="progress-description">
+                    70% Increase in 30 Days
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+
+            <div class="col-lg-3">
+                <!-- Apply any bg-* class to to the info-box to color it -->
+                <div class="info-box bg-green">
+                <span class="info-box-icon"><i class="fab fa-blackberry"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Retur</span>
+                    <span class="info-box-number">41,410</span>
+                    <!-- The progress section is optional -->
+                    <div class="progress">
+                    <div class="progress-bar" style="width: 10%"></div>
+                    </div>
+                    <span class="progress-description">
+                    10% Increase in 30 Days
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+
+            <div class="col-lg-3">
+                <!-- Apply any bg-* class to to the info-box to color it -->
+                <div class="info-box bg-blue">
+                <span class="info-box-icon"><i class="fas fa-shipping-fast"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Order</span>
+                    <span class="info-box-number">71,410</span>
+                    <!-- The progress section is optional -->
+                    <div class="progress">
+                    <div class="progress-bar" style="width: 90%"></div>
+                    </div>
+                    <span class="progress-description">
+                    90% Increase in 30 Days
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+
+            <div class="col-lg-3">
+                <!-- Apply any bg-* class to to the info-box to color it -->
+                <div class="info-box bg-orange">
+                <span class="info-box-icon"><i class="fas fa-home"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Order</span>
+                    <span class="info-box-number">71,410</span>
+                    <!-- The progress section is optional -->
+                    <div class="progress">
+                    <div class="progress-bar" style="width: 90%"></div>
+                    </div>
+                    <span class="progress-description">
+                    90% Increase in 30 Days
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+
+        </div>
+    <!-- </div> -->
+</template>
+<script>
+export default {
+    mounted(){
+        console.log('Component Mounted')
+    }
+}
+</script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>Admin BisAPI</title>
 
 <link rel="stylesheet" href="/css/app.css">
 </head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<body class="hold-transition sidebar-mini" >
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -78,10 +79,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
            <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>Dashboard</p>
-            </a>
+            </router-link>
           </li>
 
           <li class="nav-item has-treeview ">
@@ -109,17 +110,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-server"></i>
+            <router-link to="/webservice" class="nav-link">
+              <i class="nav-icon fab fa-blackberry"></i>
               <p>Webservices</p>
-            </a>
+            </router-link>
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/profile" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>Profile</p>
-            </a>
+            </router-link>
           </li>
 
 
@@ -152,48 +153,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
-          <div class="row">
-            <div class="info-box col-3">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-red"><i class="fas fa-star"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">93,139</span>
-            </div>
-            <!-- /.info-box-content -->
-            </div>
-
-            <div class="info-box col-3">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-green"><i class="fas fa-comments"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">93,139</span>
-            </div>
-            <!-- /.info-box-content -->
-            </div>
-
-            <div class="info-box col-3">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-blue"><i class="fas fa-shipping-fast"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">93,139</span>
-            </div>
-            <!-- /.info-box-content -->
-            </div>
-
-            <div class="info-box col-3">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-blue"><i class="fas fa-dolly-flatbed"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">93,139</span>
-            </div>
-            <!-- /.info-box-content -->
-            </div>
-
-          </div>
+          
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
@@ -201,9 +161,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <div class="row">
+        
           <!-- /.col-md-6 -->
-        </div>
+          <router-view></router-view>
+        
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
